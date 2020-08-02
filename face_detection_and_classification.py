a223b25385538d33a99d804316881d9c2ab146a4
PrakashDahal <53021286+PDahal2871@users.noreply.github.com>
2020-08-02 08:40:05 +0545

    detection model updated

--- face_detection_and_classification.py
+++ face_detection_and_classification.py
@@ -3,7 +3,6 @@ Importing necessary libraries
 """
 import cv2,mtcnn
 from mtcnn.mtcnn import MTCNN
-import tensorflow
 import numpy as np
 from keras.preprocessing.image import img_to_array
 from keras.applications.mobilenet_v2 import preprocess_input
@@ -13,10 +12,11 @@ warnings.filterwarnings('ignore')
 classifier = load_model('mask_detector.model') #Classifier used to predict mask or not mask
 
 
+
 #Initializing the front camera
 cap = cv2.VideoCapture(0)
 while True:
-    ret, frame = cap.read() #It read the frames from the live camera
+    _, frame = cap.read() #It read the frames from the live camera
     fcs=[]
     preds=[]
     locs=[]
@@ -27,41 +27,55 @@ while True:
     bounding boxes and use classifier to predict the faces
     """
 
-    #For Haar Cascade
-    hc = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-    hcfaces = hc.detectMultiScale(frame,1.2)
-    for (x,y,w,h) in hcfaces:
-        x2=x+w
-        y2=y+h
-        fc=frame[y:y2, x:x2] #capturing only faces
-        fc = cv2.resize(fc, (224, 224))
-        fc = img_to_array(fc) #Converting face image to array
-        fc = preprocess_input(fc)
-        fcs.append(fc)
-        locs.append((x, y, x2, y2))
+    """
+    ***Note***
+    You can choose any one of the Haar Cascade Classifier or mtcnn. If you choose one, 
+    don't forget to comment the code of another one.
+    """
 
-
-    # #For mtcnn.
+    # # --For Haar Cascade Classifier--
     # """
-    # (It requires large computation power, but gives better accuracy for face detection than
-    # Haar Cascade. Run only when u have high end laptop)
+    # It is faster than mtcnn but less accurate to detect variety of faces
     # """
-    # mt = MTCNN()
-    # faces = mt.detect_faces(frame) #it returns dictionary containing bbox, confidence and
-    # # points of eyes,nose
-    #
-    # for face in faces:
-    #     x,y,w,h = face['box']
-    #     confidence = face['confidence']
+    # hc = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+    # hcfaces = hc.detectMultiScale(frame,1.2)
+    # for (x,y,w,h) in hcfaces:
     #     x2=x+w
     #     y2=y+h
-    #     fc=frame[y:y2, x:x2]
-    #     fc= cv2.resize(fc, (224,224))
-    #     fc=img_to_array(fc)
-    #     fc= preprocess_input(fc)
+    #     fc=frame[y:y2, x:x2] #capturing only faces from the frame
+    #     fc = cv2.resize(fc, (224, 224))
+    #     fc = img_to_array(fc) #Converting face image to array
+    #     fc = preprocess_input(fc)
     #     fcs.append(fc)
     #     locs.append((x, y, x2, y2))
-    #     cfd.append(confidence)
+    # # --End of Haar Cascade Classifier code--
+
+
+    # --For mtcnn--
+    """
+    (MTCNN requires large computational power, but gives better accuracy for face detection than
+    Haar Cascade. This model is not recommended for computers having lower computing power)
+    """
+    mt = MTCNN()
+    faces = mt.detect_faces(frame) #it returns dictionary containing bbox, confidence and
+    # keypoints of eyes,nose,mouth
+
+    for face in faces:
+        x,y,w,h = face['box']
+        confidence = face['confidence']
+        x2=x+w
+        y2=y+h
+        fc=frame[y:y2, x:x2]
+        fc= cv2.resize(fc, (224,224))
+        fc=img_to_array(fc)
+        fc= preprocess_input(fc)
+        fcs.append(fc)
+        locs.append((x, y, x2, y2))
+        cfd.append(confidence)
+
+    # --End of mtcnn code--
+
+
 
     if len(fcs) > 0:
         fcs = np.array(fcs, dtype="float32") # Just ensuring that images are in array form
@@ -70,14 +84,17 @@ while True:
     for (bbox, pred) in zip(locs, preds):
         (x, y, x2, y2) = bbox
         (m, nm) = pred
+        print(pred)
         label = "Mask" if m>nm else "No Mask"
         color = (0, 255, 0) if label == "Mask" else (0, 0, 255) # green if mask else red
-        label = "{}: {:.2f}%".format(label, max(m, nm) * 100)
+        label = "{}: {:.2f}%".format(label, max(m, nm) * 100) # % of confidence
 
-        cv2.putText(frame, label, (x, y - 12),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2) #Putting text in live frames
 
-        cv2.rectangle(frame, (x, y), (x2, y2), color, 3) #Putting rectangle of bbox in frames
+        cv2.rectangle(frame, (x, y), (x2, y2), color, 3)  # Putting rectangle of bbox in frames
+        cv2.rectangle(frame, (x,y-40), (x2,y), color, -1)
+
+        cv2.putText(frame, label, (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)  # Putting text in live frames
         cv2.imshow('frame', frame)
 
 
@@ -89,6 +106,6 @@ cap.release()
 cv2.destroyAllWindows()
 
 """
-The accuracy of the classifier can be increased by changing with new more accurate classifier
+The accuracy of the classifier can be increased by changing it with a new, more accurate classifier
 or fine tuning the current one again
 """
